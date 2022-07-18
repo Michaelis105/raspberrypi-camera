5acd6dc2c9bde780fae387b3c532bb33d1f647a8
Michael Louie <michael.louie10@gmail.com>
2022-07-18 00:00:58 -0400

    Implemented clean older video parts on file system size threshold, changed logging

--- video_record.sh
+++ video_record.sh
@@ -7,7 +7,10 @@ config_path=${save_path}/my_libcamera-vid.config
 record_interval=30000
 record_width=1920
 record_height=1080
-record_name=video_part%04d.h264
+record_name=video_part
+record_format=%04d
+record_extension=.h264
+max_bytes_storage_threshold=1000000
 
 log() {
 	local timestamp="`date +\"${log_format}\"`"
@@ -32,6 +35,26 @@ is_recording() {
 	echo `ps -C libcamera-vid -o pid= `
 }
 
+delete_oldest_video_parts() {
+	local oldest_video_part_path=`ls ${save_path}/${record_name}* -t | tail -1`
+	if [[ ! -z "${oldest_video_part_path}" ]]; then
+		log "Deleting ${oldest_video_part_path}"
+		rm ${oldest_video_part_path}
+	else
+		log "Nothing to delete"
+	fi	
+}
+
+clean_oldest_video_parts_check() {
+	root_stats=`df | grep "/dev/root"`
+	used_bytes=`echo ${root_stats} | awk '{print $3}'`
+	#available_bytes=`echo ${root_stats} | awk '{print $4}'`
+	#percentage_used=`echo ${root_stats} | awk '{print $4}' | sed 's/.$//`
+	if [ "$used_bytes" -gt "$max_bytes_storage_threshold" ]; then
+		delete_oldest_video_parts
+	fi
+}
+
 log "Script start"
 
 if test -f ${config_path}; then
@@ -41,14 +64,14 @@ else
 	log "Config file not found - using default settings in script source code"
 fi
 
-log "Begin record loop"
+log "Recording start"
 
 # --flush: Write frames to disk immediately without waiting for video to stop recording.
 # --inline: Allow viewing video sequences from any frame, useful for video seeking.
 libcamera-vid \
 	-t 0 \
 	--segment ${record_interval} \
-	-o ${save_path}/${record_name} \
+	-o ${save_path}/${record_name}${record_format}${record_extension} \
 	--width ${record_width} \
 	--height ${record_height} \
 	--flush	\
@@ -61,12 +84,15 @@ sleep 2
 
 trap kill_record SIGINT
 
+log "Record loop start"
+
 while : ; do
 	is_record=`is_recording`
 	if [[ -z "${is_record}" ]]; then
-		log "End record loop..."
+		log "Record loop end"
 		break
 	fi
+	clean_oldest_video_parts_check
 	log "Alive"
 	sleep 10
 done
